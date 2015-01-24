74d299a4c0614f0330dcce4ed05589a4043d3c80
Steve Sobenko <steves@partnersandsimons.com>
2015-01-24 16:30:12 -0500

    added mapfunction

--- loadkeg1.php
+++ loadkeg1.php
@@ -1,7 +1,17 @@
 <?php
+	function mapfloat($x)
+	{
+		$loadA = 0; // kg
+	    $analogvalA = 22.91; // analog reading taken with load A on the load cell
+
+		$loadB = 55; // kg
+		$analogvalB = 415.30; // analog reading taken with load B on the load cell
+  		return (x-loadA) * (analogvalB-analogvalA)/(loadB-loadA) + analogvalA;
+	}
+
     $file = "keg1.txt";
     $f = fopen($file, "r");
     while ( $line = fgets($f, 1000) ) {
-    print $line;
+    	print "analog" + $line + " weight: " + mapfloat($line);
     }
 ?>
