529c5d3c3442173e3d7155f8246bf765911e8459
Steve Sobenko <steves@partnersandsimons.com>
2015-01-24 16:54:49 -0500

    added map function to py

--- weight.py
+++ weight.py
@@ -1,11 +1,20 @@
 #!python2.6
 import serial
 import time
+
+loadA = 0
+analogvalA = 22.91
+loadB = 55
+analogvalB = 415.30
+
+def maprange(s):
+	return  analogvalA + ((s - loadA) * (analogvalB - analogvalA) / (loadB - loadA))
+
 ser = serial.Serial('/dev/ttyACM0', 9600)
 while 1 :
 	line = ser.readline()
 	if not line.strip():
 		continue
 	f = open('keg1.txt','w')
-	f.write(line)
+	f.write(maprange(line))
 	f.close()
